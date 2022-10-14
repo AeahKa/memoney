2740f0031fa43ae3bb2f8e4ade945037769b6e16
AeahKa <aeah@qq.com>
2022-10-14 18:51:20 +0800

    将bill保存至localstorage

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -2,7 +2,7 @@
   <div>
     <Layout classPrefix="layout">
       {{ bill }}
-      <calculator @update:value="onUpdateAmount" />
+      <calculator @update:value="onUpdateAmount" @submit="submit" />
       <type :value.sync="bill.type" />
       <remark @update:value="onUpdateRemark" />
       <tags :data-source.sync="tags" @update:value="onUpdateTags" />
@@ -12,7 +12,7 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Component, Prop } from "vue-property-decorator";
+import { Component, Watch } from "vue-property-decorator";
 import Calculator from "./Add/Calculator.vue";
 import Remark from "./Add/Remark.vue";
 import Tags from "./Add/Tags.vue";
@@ -32,12 +32,14 @@ export default class Add extends Vue {
   name = "Add";
 
   tags = ["衣", "食", "住", "行"];
+  billList: Bill[] = [];
   bill: Bill = {
     tags: [],
     remark: "",
     type: "-",
     amount: 0,
   };
+
   onUpdateTags(value: string[]) {
     this.bill.tags = value;
   }
@@ -46,6 +48,16 @@ export default class Add extends Vue {
   }
   onUpdateAmount(value: string) {
     this.bill.amount = parseFloat(value);
+  }
+  submit() {
+    const billCopy = JSON.parse(JSON.stringify(this.bill));
+    this.billList.push(billCopy);
+    console.log(this.billList);
+  }
+
+  @Watch("billList")
+  onBillListChanged() {
+    window.localStorage.setItem("billList", JSON.stringify(this.billList));
   }
 }
 </script>
--- src/views/Add/Calculator.vue
+++ src/views/Add/Calculator.vue
@@ -53,6 +53,8 @@ export default class Calculator extends Vue {
   }
   save() {
     this.$emit("update:value", this.output);
+    this.$emit("submit", this.output);
+    this.output = "0";
   }
 }
 </script>
