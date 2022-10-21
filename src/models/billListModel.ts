3cea9088dc0c41800a2def63eb9561accf0e2405
AeahKa <aeah@qq.com>
2022-10-21 15:07:29 +0800

    重新封装billListModel

--- src/models/billListModel.ts
+++ src/models/billListModel.ts
@@ -1,16 +1,19 @@
 import Bill from '../custom'
 const localStorageKeyName = 'billList'
 const billListModel = {
+	data: [] as Bill[],
+
 	copy(data: Bill[] | Bill) {
 		return JSON.parse(JSON.stringify(data))
 	},
 	fetch() {
-		return JSON.parse(
+		this.data = JSON.parse(
 			window.localStorage.getItem(localStorageKeyName) || '[]'
 		) as Bill[]
+		return this.data
 	},
-	save(data: Bill[]) {
-		window.localStorage.setItem(localStorageKeyName, JSON.stringify(data))
+	save() {
+		window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data))
 	},
 }
 export default billListModel
