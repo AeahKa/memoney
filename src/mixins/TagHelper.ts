212570189c188bcfdae1b2fc3dfd85848b26e105
AeahKa <aeah@qq.com>
2022-10-28 01:04:33 +0800

    修复新建标签时点击取消后会生成空标签的bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -7,6 +7,8 @@ export class TagHelper extends Vue {
 		const name = window.prompt('请输入标签名') as string
 		if (name === '') {
 			window.alert('标签名不能为空')
+		} else if (name === null) {
+			return
 		} else {
 			this.$store.commit('newTag', name)
 		}
